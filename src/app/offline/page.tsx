6847f6de6a3c50db2b536cd1fab87d77f5c047c4
TamukongBt <briantbrian4@gmail.com>
2024-06-05 12:49:27 +0100

    Eslint resolved

--- src/app/offline/page.tsx
+++ src/app/offline/page.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const OfflineComponent = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex items-center justify-center h-screen bg-darkpurple">
       <div className="p-6 bg-white rounded shadow-md text-center">
-        <h1 className="text-2xl font-heading font-bold text-gray-700 mb-2">You are offline</h1>
-        <p className="text-gray-500 font-display">Please check your internet connection.</p>
+        <h1 className="text-2xl font-display font-bold text-whiteout mb-2">You are offline</h1>
+        <p className="text-whiteout font-sans">Please check your internet connection.</p>
       </div>
     </div>
   );
