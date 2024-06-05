d5cc4f27f3bc0e586d53ab2d5bb7b8bbe5f8298a
TamukongBt <briantbrian4@gmail.com>
2024-06-05 16:16:05 +0100

    fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 } from "next/font/google";
-import {ApolloWrapper} from "./wrapper";
+import { ApolloWrapper } from "./wrapper";
 import "./globals.css";
 import SideBar from "@/components/sidebar";
 import Header from "@/components/header";
@@ -22,24 +22,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-     
-      <body className={josefins.className}>
-        <ApolloWrapper>
-          <main className="flex min-h-screen w-full  bg-darkpurple ">
-            <div className="md:w-[7%] lg:w-[4%] sm:hidden md:block ">
-              <BottomMenubar />
-              <SideBar />
-            </div>
-            <div className=" bg-brownpurple w-full !h-full  ">
-              <Header />
-              {children}
-              <Toaster/>
-              {/* <FireflyEffect /> */}
-            </div>
-          </main>
-        </ApolloWrapper>
-      </body>
-    </html>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""></link>
+            <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Monoton&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet"/>
+            </head>
+            <body className={josefins.className}>
+              <ApolloWrapper>
+                <main className="flex min-h-screen w-full  bg-darkpurple ">
+                  <div className="md:w-[7%] lg:w-[4%] sm:hidden md:block ">
+                    <BottomMenubar />
+                    <SideBar />
+                  </div>
+                  <div className=" bg-brownpurple w-full !h-full  ">
+                    <Header />
+                    {children}
+                    <Toaster />
+                    {/* <FireflyEffect /> */}
+                  </div>
+                </main>
+              </ApolloWrapper>
+            </body>
+          </html>
 
-  );
+          );
 }
