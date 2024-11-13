ff39fd2c3846c751f2ab009eac79ffdb17179be6
TamukongBt <briantbrian4@gmail.com>
2024-11-13 23:50:36 +0100

    The second though first deploy

--- next.config.js
+++ next.config.js
@@ -5,25 +5,19 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   swcMinify: true,
   dest: "public",
   fallbacks: {
-    //image: "/static/images/fallback.png",
-    document: "/offline", // if you want to fallback to a custom page rather than /_offline
-    // font: '/static/font/fallback.woff2',
-    // audio: ...,
-    // video: ...,
+    document: "/offline",
   },
   workboxOptions: {
     disableDevLogs: true,
   },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  // ... other options you like
-});
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  // ... other options you like
 };
 
 module.exports = withPWA(nextConfig);
